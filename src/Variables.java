e62dd5990d3a716977913b12c24b25eb34d2c9c3
100147919 <100147919@lubbockisd.net>
2023-09-11 09:00:29 -0500

    Practice defining Variables

---
+++ src/Variables.java
@@ -0,0 +1,48 @@
+public class Variables {
+    public static void main(String[] args) {
+//define 1 variable of each of the
+//following data types
+//byte short int long
+//float double
+//char boolean String
+//integer variables
+        byte byteOne = 127;
+        short please = -4556;
+        int yesh = 247845;
+        long big = 123456789;
+        float ing = 1.66f;
+        double er = 124.4563;
+        char lie = 'a';
+        boolean two = false;
+        String three = " Good Morning ";
+        System.out.println("////////////////////////////////");
+        System.out.println(";*William Jackson          09/11/23 *");
+        System.out.println("*                                   *");
+        System.out.println("*   Integer Types                   *");
+        System.out.println("*                                   *");
+        System.out.println("*8 bit - byteOne = "+byteOne+"\t\t\t\t*");
+        System.out.println("* 16 bit - short please = -4556     *");
+        System.out.println("* 32 bit - int yesh = 247845        *");
+        System.out.println("* 64 bit - long big = 123456789     *");
+        System.out.println("*                                   *");
+        System.out.println("*            Real Types             *");
+        System.out.println("*                                   *");
+        System.out.println("* 32 bit - float ing = 1.66f        *");
+        System.out.println("* 64 bit - double er = 124.4563     *");
+        System.out.println("*                                   *");
+        System.out.println("*      Other Integer types          *");
+        System.out.println("*                                   *");
+        System.out.println("* 16 bit - char lie = a             *");
+        System.out.println("*                                   *");
+        System.out.println("*         Other Types               *");
+        System.out.println("*                                   *");
+        System.out.println("* booleanTwo = false                *");
+        System.out.println("* String three = Good Morning       *");
+        System.out.println("/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\");
+
+
+
+   }
+}
+
+
